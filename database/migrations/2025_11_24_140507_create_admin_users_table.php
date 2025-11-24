5211e6298d912b81787c96fc7945591475438ccc
thiri <thiriaung.java.it@gmail.com>
2025-11-24 21:10:27 +0630

    creating tables by migration

---
+++ database/migrations/2025_11_24_140507_create_admin_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address');
+            $table->string('pswd');
+            $table->boolean('is_active')->default(true);
+            $table->boolean('gender')->default(true);
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_users');
+    }
+};
