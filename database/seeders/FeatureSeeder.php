4d4df6ad319d5062e93df007ee27e82678cc0939
thiri <thiriaung.java.it@gmail.com>
2025-12-13 20:36:26 +0630

    creating featureSeeder and PermissionSeeder

---
+++ database/seeders/FeatureSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Feature;
+
+class FeatureSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+   
+    public function run(): void
+    {
+        //
+         $features = [
+            ['name' => 'user'],
+            ['name' => 'role'],
+            ['name' => 'feature'],
+            ['name' => 'permission'],
+            ['name' => 'stock'],
+            ];
+
+            foreach ($features as $feature) {
+            Feature::firstOrCreate([
+                'name' => $feature['name']
+            ]);
+        }
+    }
+}
---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+         $permissions = [
+            ['name' => 'view' ,'feature_id'=>1],
+            ['name' => 'delete','feature_id'=>1],
+            ['name' => 'edit','feature_id'=>1],
+            ['name' => 'add','feature_id'=>1],
+             ['name' => 'view' ,'feature_id'=>2],
+            ['name' => 'delete','feature_id'=>2],
+            ['name' => 'edit','feature_id'=>2],
+            ['name' => 'add','feature_id'=>2],
+             ['name' => 'view' ,'feature_id'=>3],
+            ['name' => 'delete','feature_id'=>3],
+            ['name' => 'edit','feature_id'=>3],
+            ['name' => 'add','feature_id'=>3],
+             ['name' => 'view' ,'feature_id'=>4],
+            ['name' => 'delete','feature_id'=>4],
+            ['name' => 'edit','feature_id'=>4],
+            ['name' => 'add','feature_id'=>4],
+            ];
+
+            foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permissions['name'],
+                'feature_id' => $permissions['feature_id']
+            ]);
+        }
+    }
+}
