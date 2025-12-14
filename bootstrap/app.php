ea278e3b4c2ab80376a7172d2ebb3df988316cb5
thiri <thiriaung.java.it@gmail.com>
2025-12-14 17:05:10 +0630

    adding authenticationException

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,5 +21,13 @@ return Application::configure(basePath: dirname(__DIR__))
         
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+
+         $exceptions->renderable(function (
+        AuthenticationException $e,
+        $request
+        ) {
+        // 🔴 login မဝင်ရသေးရင် 404 ပြ
+        throw new NotFoundHttpException();
+        });
         //
     })->create();
